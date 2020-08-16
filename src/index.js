6b5ea4d5f1bef0d0ca57bcc8b07cae7e1e70e344
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-08-16 02:45:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.queryElementById('create-task-form')
-  let description = document.ElementById('new-task-description')
+  let form = document.queryElementById('#create-task-form')
+  let description = document.ElementById('#new-task-description')
   let todoList = document.getElementById('tasks')
 
   form.addEventListener("submit", function(event){
@@ -9,4 +9,5 @@ document.addEventListener("DOMContentLoaded", () => {
         todoList.innerHTML += `<li> ${description.value} </li>`
         description.value = ''
       }
+
 })
